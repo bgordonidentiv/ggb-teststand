6726d53647ba2f57528e88cb6a33db75a9d10725
Bryan <bgordon@hirschsecure.com>
2025-06-03 16:50:49 -0700

    st-link working

--- stm32_flash_api/app.py
+++ stm32_flash_api/app.py
@@ -1,3 +1,20 @@
+##############################################################
+#
+# ST-LINK
+# /read
+#
+# POWER SUPPLY
+# /api/ps/device_list           = Get a list of connected devices
+# /api/ps/info                  = Get Power Supply Info
+# /api/ps/ch1/set_voltage       = Read/Write Set Voltage 
+# /api/ps/ch1/set_current       = Read/Write Set Current
+# /api/ps/ch1/measured_voltage  = Read Measured Voltage
+# /api/ps/ch1/measured_current  = Read Measured Current
+# /api/ps/ch1/on                = Read/Write ON 
+# /api/ps/ch1/off               = Read/Write OFF
+#
+##############################################################
+
 import subprocess
 import pyvisa 
 import time
@@ -8,9 +25,31 @@ app = Flask(__name__)
 
 delay = 0.5 # Delay for while communicating over USB to the power supply
 
-@app.route('/read')
+@app.route('/api/st_link/cli')
+def cli_info():
+    result = subprocess.run(['/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI'], capture_output=True, text=True)
+    output = result.stdout
+
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    clean_output = ansi_escape.sub('', output)    
+
+    # return render_template_string(formatted_output)
+    return jsonify({"output": clean_output, "error": result.stderr, "returncode": result.returncode})
+
+@app.route('/api/st_link/read', methods=["POST"])
 def read_flash():
-    result = subprocess.run(['/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI', '-c port=swd -r32 0x8000000 64'], capture_output=True, text=True)
+
+    # Get the JSON data from the request body
+    data = request.get_json()
+
+    startAddress = data.get('start_address')
+    numberOfBytes = data.get('no_of_bytes')
+
+    st_link_cli = '/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI'
+    params = f'-c port=swd -r32 {startAddress} {numberOfBytes}'
+
+    # result = subprocess.run([st_link_cli, '-c port=swd -r32 0x8000000 64'], capture_output=True, text=True)
+    result = subprocess.run([st_link_cli, params], capture_output=True, text=True)
     output = result.stdout
 
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
@@ -20,7 +59,46 @@ def read_flash():
     # return render_template_string(formatted_output)
     return jsonify({"output": clean_output, "error": result.stderr, "returncode": result.returncode})
 
-@app.route('/ps/list')
+@app.route('/api/st_link/erase')
+def erase_flash():
+
+    st_link_cli = '/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI'
+    params = f'-c port=swd -e all'
+
+    # result = subprocess.run([st_link_cli, '-c port=swd -r32 0x8000000 64'], capture_output=True, text=True)
+    result = subprocess.run([st_link_cli, params], capture_output=True, text=True)
+    output = result.stdout
+
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    clean_output = ansi_escape.sub('', output)    
+    formatted_output = f"<pre>{clean_output}</pre>"
+
+    # return render_template_string(formatted_output)
+    return jsonify({"output": clean_output, "error": result.stderr, "returncode": result.returncode})
+
+@app.route('/api/st_link/write')
+def write_flash():
+
+    st_link_cli = '/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI'
+    # port = '-c port=swd -w /home/ubuntu/GGB_LED_TEST.bin 0x8000000 -v -q -rst'
+    port = '-c port=swd'
+    bin_file = '-w /home/ubuntu/GGB_LED_TEST.bin'
+
+    result = subprocess.run([st_link_cli,
+                             "-c","port=swd",
+                             "-w","/home/ubuntu/GGB_LED_TEST.bin","0x8000000",
+                             "-v", "-q", "-rst"
+                            ], capture_output=True, text=True)
+    # result = subprocess.run(["ls", "/home/ubuntu"], capture_output=True, text=True, check=True)
+    output = result.stdout
+
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    clean_output = ansi_escape.sub('', output)    
+    formatted_output = f"<pre>{clean_output}</pre>"
+
+    return jsonify({"output": clean_output, "error": result.stderr, "returncode": result.returncode})
+
+@app.route('/api/ps/device_list')
 def list_ps():
     rm = pyvisa.ResourceManager()
     data = {
@@ -28,8 +106,7 @@ def list_ps():
     }
     return jsonify(data)
 
-
-@app.route('/ps/info')
+@app.route('/api/ps/info')
 def read_ps():
     rm = pyvisa.ResourceManager()
 
@@ -51,8 +128,8 @@ def read_ps():
     # inst.close()
     return data_out 
 
-@app.route('/ps/ch1/voltage', methods=["POST", "GET"])
-def read_volt_ch1():
+@app.route('/api/ps/ch1/set_voltage', methods=["POST", "GET"])
+def set_volt_ch1():
     rm = pyvisa.ResourceManager()
     inst=rm.open_resource("USB0::0x0483::0x7540::SPD3ECAC3R0303::INSTR")
     inst.write_termination='\n'
@@ -80,7 +157,7 @@ def read_volt_ch1():
         time.sleep(delay)
         qStr = inst.read()
 
-        return jsonify({"status": "success", "set_voltage" : float(setVoltage), "channel" : "1", "voltage" : float(qStr)}), 200
+        return jsonify({"status": "success", "set_voltage" : setVoltage, "channel" : "1", "voltage" : float(qStr)}), 200
     else:
         inst.write('CH1:VOLTage?')
         time.sleep(0.25) # I guess the power supply needs a bit of time before reading
@@ -93,8 +170,8 @@ def read_volt_ch1():
         # inst.close()
         return data_out 
 
-@app.route('/ps/ch1/current', methods=["POST", "GET"])
-def read_curr_ch1():
+@app.route('/api/ps/ch1/set_current', methods=["POST", "GET"])
+def set_curr_ch1():
     rm = pyvisa.ResourceManager()
 
     inst=rm.open_resource("USB0::0x0483::0x7540::SPD3ECAC3R0303::INSTR")
@@ -117,7 +194,7 @@ def read_curr_ch1():
         inst.write('CH1:CURRent?') # Added this read because each time I did only the write above the USB would hang
         time.sleep(delay)
         qStr = inst.read()
-        return jsonify({"status": "success", "set_current" : float(setCurrent), "channel" : "1", "current" : float(qStr)}), 200
+        return jsonify({"status": "success", "set_current" : setCurrent, "channel" : "1", "current" : float(qStr)}), 200
     else:
         inst.write('CH1:CURRent?')
         time.sleep(0.25)
@@ -129,7 +206,35 @@ def read_curr_ch1():
         data_out = jsonify(data)
         return data_out
 
-@app.route('/ps/ch1/on', methods=["POST", "GET"])
+@app.route('/api/ps/ch1/measured_voltage')
+def get_measured_voltage():
+    rm = pyvisa.ResourceManager()
+
+    inst=rm.open_resource("USB0::0x0483::0x7540::SPD3ECAC3R0303::INSTR")
+    inst.write_termination='\n'
+    inst.read_termination='\n'
+    time.sleep(0.04)
+    psMsg = 'MEASure:VOLTage? CH1'
+    inst.write(psMsg)
+    time.sleep(0.25)
+    qStr = inst.read()
+    return jsonify({"status": "success", "msg": "measured_voltage", "voltage" : float(qStr)}), 200
+
+@app.route('/api/ps/ch1/measured_current')
+def get_measured_current():
+    rm = pyvisa.ResourceManager()
+
+    inst=rm.open_resource("USB0::0x0483::0x7540::SPD3ECAC3R0303::INSTR")
+    inst.write_termination='\n'
+    inst.read_termination='\n'
+    time.sleep(0.04)
+    psMsg = 'MEASure:CURRent? CH1'
+    inst.write(psMsg)
+    time.sleep(0.25)
+    qStr = inst.read()
+    return jsonify({"status": "success", "msg": "measured_current", "current" : float(qStr)}), 200
+
+@app.route('/api/ps/ch1/on', methods=["POST", "GET"])
 def set_ch1_on():
     rm = pyvisa.ResourceManager()
 
@@ -159,7 +264,7 @@ def set_ch1_on():
             state = 'off'
         return jsonify({"status": "success", "msg": state}), 200
 
-@app.route('/ps/ch1/off', methods=["POST", "GET"])
+@app.route('/api/ps/ch1/off', methods=["POST", "GET"])
 def set_ch1_off():
     rm = pyvisa.ResourceManager()
 
